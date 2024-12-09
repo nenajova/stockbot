2a83c086dcc89e09926a26d1a0c1344ae0142593
Nenad Jovanovic <nenajova@gmail.com>
2024-12-09 11:39:27 +0100

    Stock Entity

---
+++ migrations/Version20241209102244.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241209102244 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        // $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, stock_name VARCHAR(4) NOT NULL, created DATE NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE stock (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL, open DOUBLE PRECISION NOT NULL, close DOUBLE PRECISION NOT NULL, low DOUBLE PRECISION NOT NULL, high DOUBLE PRECISION NOT NULL, adj_close DOUBLE PRECISION NOT NULL, volume INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        // $this->addSql('DROP TABLE company');
+        $this->addSql('DROP TABLE stock');
+    }
+}
---
+++ src/Entity/Stock.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StockRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StockRepository::class)]
+class Stock
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'date')]
+    private $date;
+
+    #[ORM\Column(type: 'float')]
+    private $open;
+
+    #[ORM\Column(type: 'float')]
+    private $close;
+
+    #[ORM\Column(type: 'float')]
+    private $low;
+
+    #[ORM\Column(type: 'float')]
+    private $high;
+
+    #[ORM\Column(type: 'float')]
+    private $adjClose;
+
+    #[ORM\Column(type: 'integer')]
+    private $volume;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getOpen(): ?float
+    {
+        return $this->open;
+    }
+
+    public function setOpen(float $open): self
+    {
+        $this->open = $open;
+
+        return $this;
+    }
+
+    public function getClose(): ?float
+    {
+        return $this->close;
+    }
+
+    public function setClose(float $close): self
+    {
+        $this->close = $close;
+
+        return $this;
+    }
+
+    public function getLow(): ?float
+    {
+        return $this->low;
+    }
+
+    public function setLow(float $low): self
+    {
+        $this->low = $low;
+
+        return $this;
+    }
+
+    public function getHigh(): ?float
+    {
+        return $this->high;
+    }
+
+    public function setHigh(float $high): self
+    {
+        $this->high = $high;
+
+        return $this;
+    }
+
+    public function getAdjClose(): ?float
+    {
+        return $this->adjClose;
+    }
+
+    public function setAdjClose(float $adjClose): self
+    {
+        $this->adjClose = $adjClose;
+
+        return $this;
+    }
+
+    public function getVolume(): ?int
+    {
+        return $this->volume;
+    }
+
+    public function setVolume(int $volume): self
+    {
+        $this->volume = $volume;
+
+        return $this;
+    }
+}
