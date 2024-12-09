bd11576913379b2f179bc8ee74594a139c2d5875
Nenad Jovanovic <nenajova@gmail.com>
2024-12-08 14:13:09 -1000

    api endpoint for stock

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# stockbot
+# StockBot
+
+### CURL test
+```sh
+Invoke-WebRequest -Uri http://127.0.0.1:8000/api/stock `
+>>     -Method POST `
+>>     -Headers @{ "Content-Type" = "application/json" } `
+>>     -Body '{"name": "Company Stock Name", "startDate": "2024-01-01", "endDate": "2024-12-31"}'
+```
--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StockController extends AbstractController
@@ -17,5 +18,34 @@ class StockController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/StockController.php',
         ]);
+    }
+    
+
+    /**
+     * @Route("/api/stock", name="api_stock", methods={"POST"})
+     */
+    public function stock(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['name'], $data['startDate'], $data['endDate'])) {
+            return $this->json(['error' => 'Missing required parameters'], 400);
+        }
+
+        $name = $data['name'];
+        $startDate = \DateTime::createFromFormat('d/m/Y', $data['startDate']);
+        $endDate = \DateTime::createFromFormat('d/m/Y', $data['endDate']);
+
+        if (!$startDate || !$endDate) {
+            return $this->json(['error' => 'Invalid date format. Use d/m/Y.'], 400);
+        }
+        
+        $response = [
+            'name' => $name,
+            'startDate' => $startDate->format('Y-m-d'),
+            'endDate' => $endDate->format('Y-m-d')
+        ];
+
+        return $this->json($response);
     }
 }
