a66e5ba44850c36314320bff0d6661c6beb2a92f
Nenad Jovanovic <nenajova@gmail.com>
2024-12-09 19:48:06 +0100

    Migrations fixed

--- migrations/Version20241208193908.php
+++ migrations/Version20241208193908.php
@@ -20,27 +20,27 @@ final class Version20241208193908 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, stock_name VARCHAR(4) NOT NULL, created_at DATE NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, stock_name VARCHAR(4) NOT NULL, created DATE NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 
         // Initial data for companies
         $this->addSql("INSERT INTO company 
-        (name, stock_name, created_at, description) 
+        (name, stock_name, created, description) 
         VALUES ('Apple', 'AAPL', '2024-12-08 12:00:00', 'desc')");
 
         $this->addSql("INSERT INTO company 
-        (name, stock_name, created_at, description) 
+        (name, stock_name, created, description) 
         VALUES ('Facebook', 'META', '2024-12-08 12:00:00', 'desc')");
 
         $this->addSql("INSERT INTO company 
-        (name, stock_name, created_at, description) 
+        (name, stock_name, created, description) 
         VALUES ('Netflix', 'NFLX', '2024-12-08 12:00:00', 'desc')");
 
         $this->addSql("INSERT INTO company 
-        (name, stock_name, created_at, description) 
+        (name, stock_name, created, description) 
         VALUES ('Amazon', 'AMZN', '2024-12-08 12:00:00', 'desc')");
 
         $this->addSql("INSERT INTO company 
-        (name, stock_name, created_at, description) 
+        (name, stock_name, created, description) 
         VALUES ('Google', 'GOOG', '2024-12-08 12:00:00', 'desc 4')");
     }
 
---
+++ migrations/Version20241209181755.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241209181755 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE stock (id INT AUTO_INCREMENT NOT NULL, company_id INT NOT NULL, date DATE NOT NULL, open DOUBLE PRECISION NOT NULL, close DOUBLE PRECISION NOT NULL, low DOUBLE PRECISION NOT NULL, high DOUBLE PRECISION NOT NULL, adj_close DOUBLE PRECISION NOT NULL, volume BIGINT NOT NULL, INDEX IDX_4B365660979B1AD6 (company_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE stock ADD CONSTRAINT FK_4B365660979B1AD6 FOREIGN KEY (company_id) REFERENCES company (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stock DROP FOREIGN KEY FK_4B365660979B1AD6');
+        $this->addSql('DROP TABLE stock');
+    }
+}
